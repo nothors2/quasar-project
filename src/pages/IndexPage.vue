e20275372ea0afa4774e1a988f02a6feedf43ff7
nothors2 <nothorse@gmail.com>
2022-05-09 16:05:06 +0900

    first

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page padding>
+    <q-card>
+      <q-card-section>
+        <q-btn label="click" color="orange"></q-btn
+      ></q-card-section>
+      <q-card-section class="bg-blue text-white"> My card </q-card-section>
+      <q-card-section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, iure.
+        Maiores dignissimos pari rehenderit ea omnis quibusdam aut odit, tenetur
+        esse.
+      </q-card-section>
+      <q-card-section>
+        <q-select filled :options="['test', 'test2']" v-model="selected">
+        </q-select>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  data() {
+    return {
+      selected: null,
+    };
+  },
+});
+</script>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page padding class="flex flex-center">
+    <div class="full-width" style="max-width: 350px">
+      <h5 class="q-my-md text-center">login</h5>
+      <q-card>
+        <q-card-section>
+          <form action="">
+            <q-input
+              filled
+              label="email"
+              placeholder="email@example.com"
+              class="q-mb-md"
+            >
+            </q-input>
+            <q-input
+              filled
+              label="password"
+              type="password"
+              class="q-mb-sm"
+            ></q-input>
+            <q-checkbox :model-value="true" label="remember me"></q-checkbox>
+            <q-btn label="login" class="full-width" color="primary"></q-btn>
+          </form>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+};
+</script>
